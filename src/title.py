fcbb8a8cbe3c3c54ed725153c7f31d281eb0c66c
Nicholas Crino <crino.nicholas@gmail.com>
2023-02-18 20:34:58 -0500

    Changed drill to add in code

--- src/title.py
+++ src/title.py
@@ -19,11 +19,11 @@ class Title:
         self.study_message_hover = self.config.menu_item_font_hover.render("Study", False, (0, 0, 0))
         self.study_message_rect_hover = self.study_message_hover.get_rect(center = ((WIDTH // 2), 300))
 
-        self.drill_message = self.config.menu_item_font.render("Add Drills", False, (0, 0, 0))
-        self.drill_message_rect = self.drill_message.get_rect(center = ((WIDTH // 2), 450))
+        self.add_message = self.config.menu_item_font.render("Add Drills", False, (0, 0, 0))
+        self.add_message_rect = self.add_message.get_rect(center = ((WIDTH // 2), 450))
 
-        self.drill_message_hover = self.config.menu_item_font_hover.render("Add Drills", False, (0, 0, 0))
-        self.drill_message_rect_hover = self.drill_message_hover.get_rect(center = ((WIDTH // 2), 450))
+        self.add_message_hover = self.config.menu_item_font_hover.render("Add Drills", False, (0, 0, 0))
+        self.add_message_rect_hover = self.add_message_hover.get_rect(center = ((WIDTH // 2), 450))
 
         self.custom_message = self.config.menu_item_font.render("Custom", False, (0, 0, 0))
         self.custom_message_rect = self.custom_message.get_rect(center = ((WIDTH // 2), 600))
@@ -40,30 +40,30 @@ class Title:
         surface.blit(self.title_message, self.title_message_rect)
         if self.study_message_rect.collidepoint(mouse_pos):
             surface.blit(self.study_message_hover, self.study_message_rect_hover)
-            surface.blit(self.drill_message, self.drill_message_rect)
+            surface.blit(self.add_message, self.add_message_rect)
             surface.blit(self.custom_message, self.custom_message_rect)
 
-        elif self.drill_message_rect.collidepoint(mouse_pos):
+        elif self.add_message_rect.collidepoint(mouse_pos):
             surface.blit(self.study_message, self.study_message_rect)
-            surface.blit(self.drill_message_hover, self.drill_message_rect_hover)
+            surface.blit(self.add_message_hover, self.add_message_rect_hover)
             surface.blit(self.custom_message, self.custom_message_rect)
 
         elif self.custom_message_rect.collidepoint(mouse_pos):
             surface.blit(self.study_message, self.study_message_rect)
-            surface.blit(self.drill_message, self.drill_message_rect)
+            surface.blit(self.add_message, self.add_message_rect)
             surface.blit(self.custom_message_hover, self.custom_message_rect_hover)
 
         else: 
             surface.blit(self.study_message, self.study_message_rect)
-            surface.blit(self.drill_message, self.drill_message_rect)
+            surface.blit(self.add_message, self.add_message_rect)
             surface.blit(self.custom_message, self.custom_message_rect)
         
     
     def get_collision(self, pos):
         if self.study_message_rect.collidepoint(pos):
             return STUDY
-        elif self.drill_message_rect.collidepoint(pos):
-            return DRILL
+        elif self.add_message_rect.collidepoint(pos):
+            return ADD_DRILL
         elif self.custom_message_rect.collidepoint(pos):
             return CUSTOM
         else:
